63a3f0ddb4596808a2bd745c084af4398b899f67
mijosan <mijosan@naver.com>
2020-01-15 15:08:05 +0900

    member.java push

--- Chapter06-1/src/main/java/com/rubypaper/domain/Member.java
+++ Chapter06-1/src/main/java/com/rubypaper/domain/Member.java
@@ -1,6 +1,7 @@
 package com.rubypaper.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 import lombok.Data;
@@ -9,9 +10,11 @@ import lombok.Data;
 @Entity
 public class Member {
 	@Id
+	@GeneratedValue
 	private String id;
 	private String password;
 	private String name;
 	private String role;
+	private boolean enabled;
 	
 }
